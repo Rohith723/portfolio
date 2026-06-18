055c8d280233d24f356097287c206261c008a888
Rohith723 <rohith6723@gmail.com>
2026-06-18 14:47:06 +0530

    imported skills

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import{ Navbar} from './navbar/navbar';
 import {Hero} from './hero/hero';
 import { About } from './about/about';
+import {Skills} from './skills/skills'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, Hero, About],
+  imports: [RouterOutlet, Navbar, Hero, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
