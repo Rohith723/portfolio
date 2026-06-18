9e6fc8d65cf2104c782350289ab36ad9a991e605
Rohith723 <rohith6723@gmail.com>
2026-06-18 13:52:26 +0530

    imported hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import{ Navbar} from './navbar/navbar'
+import{ Navbar} from './navbar/navbar';
+import {Hero} from './hero/hero'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
